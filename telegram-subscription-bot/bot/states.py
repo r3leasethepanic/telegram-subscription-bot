d3602dab0e7437fe1479d1c7d2fb0ce10b79bc13
r3leasethepanic <168263972+r3leasethepanic@users.noreply.github.com>
2025-08-05 13:02:36 +0300

    Delete telegram-subscription-bot directory

--- telegram-subscription-bot/bot/states.py
+++
@@ -1,7 +0,0 @@
-
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-class Form(StatesGroup):
-    email = State()
-    phone = State()
-    city = State()
--- telegram-subscription-bot/bot/utils.py
+++
@@ -1,8 +0,0 @@
-
-import re
-
-def is_valid_email(email: str) -> bool:
-    return re.match(r"[^@\s]+@[^@\s]+\.[a-zA-Z0-9]+$", email) is not None
-
-def is_valid_phone(phone: str) -> bool:
-    return re.fullmatch(r"7\d{10}", phone) is not None
